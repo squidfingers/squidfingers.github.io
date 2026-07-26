dd54ceb4193796c66efc40b0faaad14813f0b47a
Travis Beckham <howdytravis@gmail.com>
2026-07-26 13:50:17 -0500

    Classname tweaks

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,19 +3,19 @@ window.addEventListener("DOMContentLoaded", () => {
 });
 window.addEventListener("scroll", () => {
   if (window.scrollY > 64) {
-    document.body.classList.add("js-scrolled");
+    document.body.classList.add("scrolled");
   } else {
-    document.body.classList.remove("js-scrolled");
+    document.body.classList.remove("scrolled");
   }
 });
 window.addEventListener("DOMContentLoaded", () => {
-  const animationTargets = document.querySelectorAll(".animate-on-view");
+  const animationTargets = document.querySelectorAll(".view");
   if (animationTargets) {
     animationTargets.forEach(t => {
       let observer = new IntersectionObserver(e => {
         e.forEach(e => {
           if (e.isIntersecting) {
-            e.target.classList.add("js-start-animation-on-view");
+            e.target.classList.add("in-view");
             observer.unobserve(e.target);
           }
         });
