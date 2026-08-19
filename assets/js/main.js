3cd844f9956d5a1b324cfe54876d377506d8d17f
Travis Beckham <howdytravis@gmail.com>
2026-08-19 10:55:26 -0500

    Add card stacking effect to project-items

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,13 @@
 const prefersReducedMotion = window.matchMedia("(prefers-reduced-motion: reduce)").matches;
 
 // Set header height as CSS variable
-const setHeaderHeight = () => {
+function setHeaderHeight(){
   const header = document.querySelector(".main-header");
   if (header) document.documentElement.style.setProperty("--header-height", `${header.offsetHeight}px`);
 };
 
 // Add `in-view--start` to all elements with the `in-view` class, and `in-view--end` once they scroll into view
-const inView = () => {
+function inView(){
   if (prefersReducedMotion) return;
   document.querySelectorAll(".in-view").forEach(target => {
     target.classList.add("in-view--start");
@@ -18,14 +18,14 @@ const inView = () => {
           observer.unobserve(entry.target);
         }
       });
-    });
+    });//, { threshold: 0.2, rootMargin: "0px 0px -10% 0px" }
     observer.observe(target);
   });
 };
 
 // Set footer scroll progress as CSS variable
 // `--footer-progress` is 0 when the footer is at the start of being in view, and 1 when the page has been scrolled to the bottom
-const footerReveal = () => {
+function footerReveal(){
   const footer = document.querySelector(".main-footer");
   if (prefersReducedMotion || !footer) return;
   const distanceFromBottom = document.documentElement.scrollHeight - window.innerHeight - window.scrollY;
@@ -34,7 +34,7 @@ const footerReveal = () => {
 };
 
 // Add `scrolled` class to body once the window has scrolled enough to trigger the header to collapse
-const updateScrolled = () => {
+function updateScrolled(){
   document.body.classList.toggle("scrolled", window.scrollY > 64);
 };
 
